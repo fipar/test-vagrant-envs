be45b613e7e0aada7bb7ef6b260ab6e7339a2e1e
Fernando Ipar <fipar@acm.org>
2014-06-23 19:39:47 -0300

    added Percona Server 5.6 test box, added percona-toolkit to the other boxes

--- vagrant-percona-server-5.1/provision.sh
+++ vagrant-percona-server-5.1/provision.sh
@@ -2,6 +2,6 @@
 rpm -qa|grep percona-release && exit 0 || {
 	yum -y install http://www.percona.com/downloads/percona-release/percona-release-0.0-1.x86_64.rpm
 	yum -y install Percona-Server-server-51 Percona-Server-shared-51 Percona-Server-devel-51 Percona-Server-test-51 Percona-Server-client-51
-	yum -y install percona-xtrabackup
+	yum -y install percona-xtrabackup percona-toolkit
 	service mysql start
 }
--- vagrant-percona-server-5.6/provision.sh
+++ vagrant-percona-server-5.6/provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rpm -qa|grep percona-release && exit 0 || {
 	yum -y install http://www.percona.com/downloads/percona-release/percona-release-0.0-1.x86_64.rpm
-	yum -y install Percona-Server-server-55 Percona-Server-shared-55 Percona-Server-devel-55 Percona-Server-test-55 Percona-Server-client-55
+	yum -y install Percona-Server-server-56 Percona-Server-shared-56 Percona-Server-devel-56 Percona-Server-test-56 Percona-Server-client-56
 	yum -y install percona-xtrabackup percona-toolkit
 	service mysql start
 }
