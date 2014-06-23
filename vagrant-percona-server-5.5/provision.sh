20d9f7812d2635f775bbe0471f8bff79f60e39ab
Fernando Ipar <fipar@acm.org>
2014-06-23 19:27:53 -0300

    added percona xtrabackup

--- vagrant-percona-server-5.5/provision.sh
+++ vagrant-percona-server-5.5/provision.sh
@@ -2,5 +2,6 @@
 rpm -qa|grep percona-release && exit 0 || {
 	yum -y install http://www.percona.com/downloads/percona-release/percona-release-0.0-1.x86_64.rpm
 	yum -y install Percona-Server-server-55 Percona-Server-shared-55 Percona-Server-devel-55 Percona-Server-test-55 Percona-Server-client-55
+	yum -y install percona-xtrabackup
 	service mysql start
 }
