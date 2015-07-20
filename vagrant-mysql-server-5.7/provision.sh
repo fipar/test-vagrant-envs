3a40582f8c4fc3541f1c39e91cbb946139792997
Fernando Ipar <fipar@acm.org>
2015-07-20 10:40:21 -0300

    fixed provisioning for 5.7

--- vagrant-mysql-server-5.7/provision.sh
+++ vagrant-mysql-server-5.7/provision.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 rpm -qa|grep mysql-community-release && exit 0 || {
 	yum -y install http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm
-        yum -y install http://www.percona.com/downloads/percona-release/percona-release-0.0-1.x86_64.rpm
-	yum -y install http://dev.mysql.com/get/Downloads/MySQL-5.7/MySQL-test-5.7.4_m14-1.el6.i686.rpm http://dev.mysql.com/get/Downloads/MySQL-5.7/MySQL-devel-5.7.4_m14-1.el6.i686.rpm http://dev.mysql.com/get/Downloads/MySQL-5.7/MySQL-client-5.7.4_m14-1.el6.i686.rpm http://dev.mysql.com/get/Downloads/MySQL-5.7/MySQL-server-5.7.4_m14-1.el6.i686.rpm http://dev.mysql.com/get/Downloads/MySQL-5.7/MySQL-shared-5.7.4_m14-1.el6.i686.rpm
-	yum -y install percona-xtrabackup percona-toolkit
-	service mysql start
+	yum-config-manager --enable mysql57-community-dmr
+	yum -y install mysql-server mysql-client
+        #yum -y install http://www.percona.com/downloads/percona-release/percona-release-0.0-1.x86_64.rpm
+	#yum -y install percona-xtrabackup percona-toolkit
+	service mysqld start
 }
