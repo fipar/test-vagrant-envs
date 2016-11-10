cb649a32fa145ead7f29555e0a34c47095081414
Fernando Ipar <fipar@acm.org>
2016-11-10 12:12:53 -0300

    updated percona repo

--- vagrant-percona-server-5.6/provision.sh
+++ vagrant-percona-server-5.6/provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rpm -qa|grep percona-release && exit 0 || {
-	yum -y install http://www.percona.com/downloads/percona-release/percona-release-0.0-1.x86_64.rpm
+	yum -y install http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm 
 	yum -y install Percona-Server-server-56 Percona-Server-shared-56 Percona-Server-devel-56 Percona-Server-test-56 Percona-Server-client-56
 	yum -y install percona-xtrabackup percona-toolkit
 	service mysql start
