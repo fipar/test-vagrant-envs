c7d5b1189ad3a63e9c5a816d870058b137b3c13e
Fernando Ipar <fipar@acm.org>
2017-03-02 13:14:32 -0300

    fixed provisioner as 5.7 became GA, removed public networking

--- vagrant-mysql-server-5.7/provision.sh
+++ vagrant-mysql-server-5.7/provision.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 rpm -qa|grep mysql-community-release && exit 0 || {
-	yum -y install http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm
-	yum-config-manager --enable mysql57-community-dmr
-	yum -y install mysql-server mysql-client
+	yum -y install http://dev.mysql.com/get/mysql57-community-release-el6-9.noarch.rpm
+	yum -y install mysql-community-server
         #yum -y install http://www.percona.com/downloads/percona-release/percona-release-0.0-1.x86_64.rpm
 	#yum -y install percona-xtrabackup percona-toolkit
 	service mysqld start
