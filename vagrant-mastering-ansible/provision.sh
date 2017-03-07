776e9b0784cf57f66708336b9d410ab086344e9c
Fernando Ipar <fipar@acm.org>
2017-03-07 18:59:47 -0300

    changed to Ubuntu as that is what is used in the course

--- vagrant-mastering-ansible/provision.sh
+++ vagrant-mastering-ansible/provision.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
 hostname | grep control > /dev/null && {
-    rpm -qi epel-release > /dev/null || rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
-    rpm -qi ansible > /dev/null || yum -y install ansible
+    dpkg -L ansible &> /dev/null || {
+	    apt-get -y install software-properties-common
+	    apt-add-repository ppa:ansible/ansible
+	    apt-get update
+	    apt-get -y install ansible
+		}
     test -f /home/vagrant/.ssh/mastering_ansible.pub || {
 	    cp /vagrant/mastering* /home/vagrant/.ssh/
 	    chown -R vagrant /home/vagrant/.ssh
@@ -15,7 +18,9 @@ hostname | grep control > /dev/null && {
 IP_BLOCK=$(grep IP_BLOCK /vagrant/Vagrantfile |awk -F'=' '{print $2}'|tr -d '"'|head -1)
 machine_id=2
 
-[ $(wc -l /etc/hosts | awk '{print $1}') -le 3 ] && {
+verification_host=lb01
+[ "$HOSTNAME" == "lb01" ] && verification_host=control
+grep $verification_host /etc/hosts >/dev/null || {
 	for name in control lb01 app01 app02 db01; do
 		echo "${IP_BLOCK}.${machine_id} $name" >> /etc/hosts
 		machine_id=$((machine_id+1))
